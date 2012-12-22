39160e407c14b3b718109664992b55aae3d3e5bc
Andrea Campi <andrea.campi@zephirworks.com>
2012-12-22 15:38:09 +0100

    For a better test, also set the default version to the only one that is available.

--- test/kitchen/cookbooks/cloudfoundry-mongodb-service_test/recipes/node_with_mongodb22.rb
+++ test/kitchen/cookbooks/cloudfoundry-mongodb-service_test/recipes/node_with_mongodb22.rb
@@ -1,3 +1,6 @@
 include_recipe "cloudfoundry-mongodb-service::install22"
 include_recipe "cloudfoundry-mongodb-service_test::node"
+
+node.default['cloudfoundry_mongodb_service']['node']['default_version'] = "2.2"
+
 include_recipe "cloudfoundry-mongodb-service::node"
