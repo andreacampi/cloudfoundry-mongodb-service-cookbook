fd9ddd9d36b2c03a80d31b23985acc309a520ee4
Andrea Campi <andrea.campi@zephirworks.com>
2013-01-01 00:42:00 +0100

    Release 1.0.2: misc improvements and fixes.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cashion@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures cloudfoundry-mongodb-service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.1"
+version          "1.0.2"
 
 %w( ubuntu ).each do |os|
   supports os
@@ -12,4 +12,4 @@ end
 %w( cloudfoundry mongodb ).each do |cb|
   depends cb
 end
-depends "cloudfoundry_service", "~> 1.1.0"
+depends "cloudfoundry_service", "~> 1.1.4"
